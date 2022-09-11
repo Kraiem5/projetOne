65fba9e3ccd67b22458b88b110c5b73a46f5819f
Kraiem5 <kraiemabid300@gmail.com>
2022-09-11 21:38:41 +0200

    m

--- src/app/shared/interfaces/services/panier.service.ts
+++ src/app/shared/interfaces/services/panier.service.ts
@@ -10,25 +10,12 @@ export class PanierService {
   public ingredients$ : BehaviorSubject<Ingredient[]> = new BehaviorSubject(null);
 
   constructor() { }
-  public addToPanier(ingredients: Ingredient[]): void {
+  public addToPanier(ingredients: Ingredient): void {
     const currentValue = this.ingredients$.value;
-    if (currentValue) {
-      const obj = [...currentValue , ...ingredients].reduce((acc, value) => {
-        if (acc[value.name]) {
-          acc[value.name] += value.quantity;
-        } else {
-          acc[value.name] = value.quantity
-        }
-        return acc
-      }, {});
-      const result = Object.keys(obj).map((key) => ({
-        name: [key],
-        quantity: obj[key],
-      }));
-      this.ingredients$.next(result);
+      let p=currentValue.push(value)
+      this.ingredients$.next(currentValue);
     }
-    else {
-      this.ingredients$.next(ingredients)
-    }
-  }
+   
+    
+  
 }
